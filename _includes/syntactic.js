90b265a60a66fafa0bf6c44412431a0eddcfeecd
syntacticsolutions <syntacticsolutions@gmail.com>
2016-07-02 14:07:25 -0700

    --all

---
+++ _includes/syntactic.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+  $(".nav-icon").click(function(){
+    $("body").toggleClass("sideOpen");
+  });
+});
+
+$(function(){
+ var obj = document.querySelectorAll('.nav-icon');
+  for(var i = obj.length -1;i>=0;i--){
+      var toggle = obj[i];
+      toggleactive(toggle);
+  }
+  
+  function toggleactive(toggle) {
+    toggle.addEventListener("click",function() {
+      
+      if(this.classList.contains("active") === true) {
+        this.classList.remove("active");
+      }
+      else {
+        this.classList.add("active");
+      }
+    });
+  }
+});
+
